5ea55b2635c5cdc2d56093b78a8aa5547987c3a9
Anto Jebikshan <dantojebi@gmail.com>
2025-01-11 15:31:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main() {
     cout<<"hello"<<"\n";
-    int a = 10, c = 9000;
+    int a = 10, c = 78;
     char b ='a';
-    cout<<a+c;
+    cout<<a+c<<"\n";
+    cout<<c*2 ;
     return 0;
 }
